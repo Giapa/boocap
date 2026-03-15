604973294cc2450f0cff3e513afb3d70ddfc9805
Giapa <giapa@protonmail.com>
2026-03-15 17:02:18 +0200

    feat: add electronAPI types

--- renderer/env.d.ts
+++ renderer/env.d.ts
@@ -1,7 +1,15 @@
 /// <reference types="vite/client" />
 
+import type { ElectronAPI } from "../shared/types/electron-api";
+
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
   const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>;
   export default component;
+}
+
+declare global {
+  interface Window {
+    electronAPI: ElectronAPI;
+  }
 }
